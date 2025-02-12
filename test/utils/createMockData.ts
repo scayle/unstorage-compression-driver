76133956e8fff619106d7679988b525306853839
Connor Pearson <connor.pearson@scayle.com>
2025-02-12 09:48:15 +0100

    chore: remove faker-js/faker

--- test/utils/createMockData.ts
+++ test/utils/createMockData.ts
@@ -1,16 +1,42 @@
-import { faker } from '@faker-js/faker'
-
-faker.seed(2023)
-
-export const createMockProductData = () => ({
-  _id: faker.string.uuid(),
-  price: faker.commerce.price({ min: 10, max: 200, dec: 2, symbol: '€' }),
-  productName: faker.commerce.product(),
-  productDescription: faker.commerce.productDescription(),
-  productMaterial: faker.commerce.productMaterial(),
-})
-
-export const createMockProductDataList = (length = 5) =>
-  Array(length)
-    .fill('')
-    .map(() => createMockProductData())
+export const createMockProductDataList = () => [
+  {
+    _id: '5e92270b-8878-4625-b252-60935621736e',
+    price: '€156.45',
+    productName: 'Shoes',
+    productDescription:
+      'The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality',
+    productMaterial: 'Steel',
+  },
+  {
+    _id: '1d4468a1-f6ba-432b-ad61-e5629dc1e757',
+    price: '€198.99',
+    productName: 'Pants',
+    productDescription:
+      'New range of formal shirts are designed keeping you in mind. With fits and styling that will make you stand apart',
+    productMaterial: 'Concrete',
+  },
+  {
+    _id: '8509e4d8-0580-4b3b-ba5a-8e897b1be8a6',
+    price: '€79.39',
+    productName: 'Salad',
+    productDescription:
+      'The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients',
+    productMaterial: 'Cotton',
+  },
+  {
+    _id: '2d75e770-1804-469d-a2c4-ce3f4eb03dad',
+    price: '€27.49',
+    productName: 'Chicken',
+    productDescription:
+      'The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J',
+    productMaterial: 'Metal',
+  },
+  {
+    _id: '64355391-bb9b-4bca-9c9d-76416a45aa58',
+    price: '€151.05',
+    productName: 'Keyboard',
+    productDescription:
+      'Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support',
+    productMaterial: 'Wooden',
+  },
+]
