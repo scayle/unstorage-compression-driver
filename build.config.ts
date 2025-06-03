17d620e082c635bbc0f120c369e7c73b52b05128
Connor Pearson <connor.pearson@scayle.com>
2025-06-03 10:57:02 +0200

    feat: drop CJS support in unstorage drivers

--- build.config.ts
+++ build.config.ts
@@ -4,7 +4,7 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   declaration: true,
   rollup: {
-    emitCJS: true,
+    emitCJS: false,
     esbuild: {
       minify: false,
     },
